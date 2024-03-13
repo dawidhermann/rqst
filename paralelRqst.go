94eb9696e558b23c6eda7e42dea1b0ae24ab1a18
Dawid Hermann <hermann.dawid.91@gmail.com>
2024-03-13 20:09:25 +0100

    Add comments

--- paralelRqst.go
+++ paralelRqst.go
@@ -5,22 +5,26 @@ import (
 	"sync"
 )
 
+// Result of a single request
 type ParalelResult struct {
 	Result any
 	Error  error
 }
 
+// Helper for executing multiple requests at once
 type ParalelRqst struct {
 	executor *http.Client
 	requests []*ParalelRequestConfig
 }
 
+// Configuration for a single paralel request
 type ParalelRequestConfig struct {
 	Id            string
 	CreateRequest func() *http.Request
 	ResultMapper  func(response *http.Response) (any, error)
 }
 
+// Creates new ParalelRqst
 func NewParalel(requestExecutor *http.Client) *ParalelRqst {
 	return &ParalelRqst{
 		executor: requestExecutor,
@@ -28,16 +32,19 @@ func NewParalel(requestExecutor *http.Client) *ParalelRqst {
 	}
 }
 
+// Add new paralel request to execution list
 func (rqst *ParalelRqst) AddNextParalelRequest(requestConfig *ParalelRequestConfig) *ParalelRqst {
 	rqst.requests = append(rqst.requests, requestConfig)
 	return rqst
 }
 
+// Add multiple new requests to execution list
 func (rqst *ParalelRqst) AddNextMultipleParalelRequests(requestConfigs ...*ParalelRequestConfig) *ParalelRqst {
 	rqst.requests = append(rqst.requests, requestConfigs...)
 	return rqst
 }
 
+// Execute all requests at once
 func (rqst *ParalelRqst) Execute() map[string]ParalelResult {
 	results := make(map[string]ParalelResult)
 	var wg sync.WaitGroup
--- rqst.go
+++ rqst.go
@@ -1,14 +1,17 @@
+// Request helper
 package rqst
 
 import (
 	"net/http"
 )
 
+// Rqst is basic struct for helping transform chain of requests
 type Rqst struct {
 	executor *http.Client
 	requests []*RequestConfig
 }
 
+// A single request configuration
 type RequestConfig struct {
 	CreateRequest func(data any) *http.Request
 	ResultMapper  func(response *http.Response) (any, error)
@@ -16,6 +19,7 @@ type RequestConfig struct {
 	MappedResultInterceptor func(mappedData any)
 }
 
+// Create new Rqst
 func New(requestExecutor *http.Client) *Rqst {
 	return &Rqst{
 		executor: requestExecutor,
@@ -23,16 +27,19 @@ func New(requestExecutor *http.Client) *Rqst {
 	}
 }
 
+// Add next request to execution chain
 func (rqst *Rqst) AddNextRequest(requestConfig *RequestConfig) *Rqst {
 	rqst.requests = append(rqst.requests, requestConfig)
 	return rqst
 }
 
+// Add multiple requests to execution chain at once
 func (rqst *Rqst) AddNextMultipleRequests(requestConfigs ...*RequestConfig) *Rqst {
 	rqst.requests = append(rqst.requests, requestConfigs...)
 	return rqst
 }
 
+// Execute request chain
 func (rqst *Rqst) Execute() any {
 	var lastResult any
 	for _, requestConfig := range rqst.requests {
